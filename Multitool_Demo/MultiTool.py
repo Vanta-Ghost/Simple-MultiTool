ae042c18fed240f69fbc0c22aa27a7aef34ddca3
Akshay Suresh <akshaysuresh1117@gmail.com>
2025-06-04 18:35:30 +0530

    Update MultiTool.py
    added ip lookup option

--- Multitool_Demo/MultiTool.py
+++ Multitool_Demo/MultiTool.py
@@ -34,6 +34,21 @@ while True:
         ip = input("Enter IP address:")
         os.system("cls")
         r = request.get(f"http://ip-api.com/json/{ip}")
+        data = r.json()
+        print("RESULTS\n")
+        print(f"\033[38;2;0;255;0;12mCountry:\033[0m  {data["country"]}")
+        print(f"\033[38;2;0;255;0;12mCity:\033[0m  {data["city"]}")
+        print(f"\033[38;2;0;255;0;12mRegion:\033[0m  {data["regionName"]}")
+        print(f"\033[38;2;0;255;0;12mTimeZone:\033[0m  {data["timezone"]}")
+        print("")
+        pause = input("Press enter to return...")
+        
+    if x == "2":
+        os.system("cls")
+        print("IP Webhook Sender\n")
+        url = input("Webhook url: ")
+        message = input("message: ")
+        name = input("webhook name: ")
         
         
 
